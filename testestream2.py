7354c5b99fed778e4d554ef12c759424776aaaa3
danyafoito-blip <danyafoito@gmail.com>
2026-04-01 11:59:57 +0100

    saaf

--- testestream2.py
+++ testestream2.py
@@ -371,7 +371,7 @@ with tab_check:
         checklist_litio(deposito_selecionado)
         
     elif recurso_selecionado == "Urânio (Combustíveis nucleares)":
-        checklist_uranio()
+        checklist_uranio(tipo_especifico)
         
     elif recurso_selecionado == "Tório (Combustíveis nucleares)":
         checklist_torio()
